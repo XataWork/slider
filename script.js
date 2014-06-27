17d0d8a942a55c2b38b65f048b31a9a7576bb034
boozz <shapovdu@yandex.ru>
2014-06-27 21:56:31 +0300

    первый commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 /**
  * Created by boozz on 27.06.14.
  */
+var slider = {
+    sliderElem: document.getElementsByClassName('slider'),
+    sliderImages: document.getElementsByTagName('ul')
+}
+console.log(slider.sliderImages);
+document.getElementsByClassName('slider');
