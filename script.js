5e9df02836b928b8f4070fa3ddcb59837270627e
boozz <shapovdu@yandex.ru>
2014-06-27 22:38:48 +0300

    первый commit

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 /**
  * Created by boozz on 27.06.14.
  */
-var slider = {
-    sliderElem: document.getElementsByClassName('slider'),
-    sliderImages: document.getElementsByTagName('ul')
-}
-console.log(slider.sliderImages);
-document.getElementsByClassName('slider');
+/*Тут будет код слайдера
+*   Пока набросал на jQuery, для понимания шагов выполнения
+* */
+//var slider = {
+//    sliderElem: document.getElementsByClassName('slider'),
+//    sliderImages: document.getElementsByTagName('ul')
+//}
+//document.getElementsByClassName('slider');
+$(document).ready(function(){
+    var slider = $('.slider');
+//    var sliderWindow = $('.slider-window');
+    var sliderImages = $('.window-slider ul');
+    var imageWidth = $('.window-slider ul li').width();
+    var colElements = $('.window-slider ul li').length;
+    sliderImages.width(imageWidth*colElements);
+    $('.controls ul li').on('click',function(){
+        $(this).siblings('li').removeClass('active');
+        $(this).addClass('active');
+        var number = $(this).attr('id'),
+            marg = -(number-1)*imageWidth;
+        sliderImages.animate({
+            "margin-left": marg
+        },1000);
+    });
+});
